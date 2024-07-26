8374eb4796affddbb0ae517b086a47d5b42653a6
RojanRegmi <rjnrcks@gmail.com>
2024-07-26 18:02:23 +0200

    trainer xla transform changes

--- trainer_xla.py
+++ trainer_xla.py
@@ -140,7 +140,7 @@ def _mp_fn(rank, flags):
     batch_size = 1024
     cifar_10_dir = '/kaggle/input/cifar10-python/cifar-10-batches-py/'
     
-    trainset = CIFAR10(data_dir=cifar_10_dir, transform=transform_test)
+    trainset = CIFAR10(data_dir=cifar_10_dir, transform=transform_train)
     testset = CIFAR10(data_dir=cifar_10_dir, train=False, transform=transform_test)
 
     train_sampler = torch.utils.data.distributed.DistributedSampler(
