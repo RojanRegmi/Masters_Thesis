709a3c69f33cabbb8f79ce5043aff7119b5f6fdc
RojanRegmi <rjnrcks@gmail.com>
2024-07-27 23:02:20 +0200

    XLA start method updated

--- trainer_xla.py
+++ trainer_xla.py
@@ -174,7 +174,7 @@ def _mp_fn(rank, flags):
 if __name__ == '__main__':
     flags = {}
     flags['num_epochs'] = 50
-    xmp.spawn(_mp_fn, args=(flags,))
+    xmp.spawn(_mp_fn, args=(flags,), start_method='fork')
 
 
 
