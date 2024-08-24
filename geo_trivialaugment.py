eacf06771e605b6bb22af38d6da64ebd4e5f9de1
RojanRegmi <rjnrcks@gmail.com>
2024-08-24 12:46:29 +0545

    Geometric Trivial Augment Wide

---
+++ geo_trivialaugment.py
@@ -0,0 +1,62 @@
+import math 
+from enum import Enum 
+from typing import Dict, List, Optional, Tuple
+
+import torch
+from torch import tensor
+
+import torchvision.transforms.functional as F 
+from torchvision.transforms.functional import InterpolationMode
+
+class GeometricTrivialAugmentWide(torch.nn.Module):
+    def __init__(
+        self,
+        num_magnitude_bins: int = 31,
+        interpolation: InterpolationMode = InterpolationMode.NEAREST,
+        fill: Optional[List[float]] = None,
+    ) -> None:
+        super().__init__()
+        self.num_magnitude_bins = num_magnitude_bins
+        self.interpolation = interpolation
+        self.fill = fill
+
+    def _augmentation_space(self, num_bins: int) -> Dict[str, Tuple[Tensor, bool]]:
+        return {
+            # op_name: (magnitudes, signed)
+            "Identity": (torch.tensor(0.0), False),
+            "ShearX": (torch.linspace(0.0, 0.99, num_bins), True),
+            "ShearY": (torch.linspace(0.0, 0.99, num_bins), True),
+            "TranslateX": (torch.linspace(0.0, 32.0, num_bins), True),
+            "TranslateY": (torch.linspace(0.0, 32.0, num_bins), True),
+            "Rotate": (torch.linspace(0.0, 135.0, num_bins), True),
+        }
+
+    def forward(self, img: Tensor) -> Tensor:
+        fill = self.fill
+        channels, height, width = F.get_dimensions(img)
+        if isinstance(img, Tensor):
+            if isinstance(fill, (int, float)):
+                fill = [float(fill)] * channels
+            elif fill is not None:
+                fill = [float(f) for f in fill]
+
+        op_meta = self._augmentation_space(self.num_magnitude_bins)
+        op_index = int(torch.randint(len(op_meta), (1,)).item())
+        op_name = list(op_meta.keys())[op_index]
+        magnitudes, signed = op_meta[op_name]
+        magnitude = (
+            float(magnitudes[torch.randint(len(magnitudes), (1,), dtype=torch.long)].item())
+            if magnitudes.ndim > 0
+            else 0.0
+        )
+        if signed and torch.randint(2, (1,)):
+            magnitude *= -1.0
+
+        return F.apply_op(img, op_name, magnitude, interpolation=self.interpolation, fill=fill)
+
+    def __repr__(self) -> str:
+        s = (f"{self.__class__.__name__}("
+             f"num_magnitude_bins={self.num_magnitude_bins}, "
+             f"interpolation={self.interpolation}, "
+             f"fill={self.fill})")
+        return s
