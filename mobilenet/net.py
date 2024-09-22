80c31d8416629d8aa1846db43cd527faae49c67d
RojanRegmi <rjnrcks@gmail.com>
2024-09-22 21:00:55 +0200

    MobileNet only until layer 8

--- mobilenet/net.py
+++ mobilenet/net.py
@@ -61,10 +61,10 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.enc_layers = list(encoder.children())
         # Define encoder stages corresponding to different layers
-        self.enc_1 = nn.Sequential(*self.enc_layers[:2])
-        self.enc_2 = nn.Sequential(*self.enc_layers[2:4])
-        self.enc_3 = nn.Sequential(*self.enc_layers[4:7])
-        self.enc_4 = nn.Sequential(*self.enc_layers[7:10])
+        self.enc_1 = nn.Sequential(*self.enc_layers[:1])
+        self.enc_2 = nn.Sequential(*self.enc_layers[1:3])
+        self.enc_3 = nn.Sequential(*self.enc_layers[3:5])
+        self.enc_4 = nn.Sequential(*self.enc_layers[5:8])
 
         self.decoder = decoder
         self.mse_loss = nn.MSELoss()
