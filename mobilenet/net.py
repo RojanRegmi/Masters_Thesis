699eb47a5655ddc06cd75da6e7e49231b17ed464
RojanRegmi <rjnrcks@gmail.com>
2024-09-29 11:15:05 +0200

    Enc_5 layers

--- mobilenet/net.py
+++ mobilenet/net.py
@@ -22,6 +22,53 @@ decoder = nn.Sequential(
     nn.Conv2d(64, 3, (3, 3)),  # Output 3-channel RGB image
 )
 
+class MobileNetv1(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
+
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+    
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+
+        self.model = nn.Sequential(
+            conv_bn(  3,  32, 2), 
+            conv_dw( 32,  64, 1),
+            conv_dw( 64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
+            nn.AvgPool2d(7),
+        )
+        self.fc = nn.Linear(1024, 1000)
+
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(-1, 1024)
+        x = self.fc(x)
+        return x
+
 
 """
 
@@ -64,7 +111,7 @@ class Net(nn.Module):
         self.mse_loss = nn.MSELoss()
 
         # Freeze encoder parameters
-        for name in ['enc_1', 'enc_2', 'enc_3', 'enc_4']:
+        for name in ['enc_1', 'enc_2', 'enc_3', 'enc_4', 'enc_5']:
             for param in getattr(self, name).parameters():
                 param.requires_grad = False
 
