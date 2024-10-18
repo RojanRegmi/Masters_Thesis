85212054b3b01baec184a55d99e2e5bcb799a47c
RojanRegmi <rjnrcks@gmail.com>
2024-10-18 14:10:52 +0200

    style transfer decoder weights

---
+++ gen_data_utils.py
@@ -0,0 +1,99 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+from torch.utils.data import Subset
+import torchvision.transforms.functional as TF
+import numpy as np
+
+class AugmentedDataset(torch.utils.data.Dataset):
+    """Dataset wrapper to perform augmentations and allow robust loss functions."""
+
+    def __init__(self, images, labels, sources, transforms_preprocess, transforms_basic, transforms_augmentation,
+                 transforms_generated=None, robust_samples=0):
+        self.images = images
+        self.labels = labels
+        self.sources = sources
+        self.preprocess = transforms_preprocess
+        self.transforms_basic = transforms_basic
+        self.transforms_augmentation = transforms_augmentation
+        self.transforms_generated = transforms_generated if transforms_generated else transforms_augmentation
+        self.robust_samples = robust_samples
+
+    def __getitem__(self, i):
+        x = self.images[i]
+        aug_strat = self.transforms_augmentation if self.sources[i] == True else self.transforms_generated
+        augment = transforms.Compose([self.transforms_basic, aug_strat])
+        if self.robust_samples == 0: # use this
+          return augment(x), self.labels[i]
+        elif self.robust_samples == 1:
+          im_tuple = (self.preprocess(x), augment(x))
+          return im_tuple, self.labels[i]
+        elif self.robust_samples == 2:
+          im_tuple = (self.preprocess(x), augment(x), augment(x))
+          return im_tuple, self.labels[i]
+
+    def __len__(self):
+        return len(self.labels)
+
+def load_augmented_traindata(base_trainset, target_size, style_transfer, seed=0, transforms_generated = None, generated_ratio = 0.5, robust_samples=0):
+
+        transforms_generated = transforms_generated
+        robust_samples = robust_samples
+        target_size = target_size
+        generated_dataset = np.load(f'/kaggle/input/cifar10-1m-npz/1m.npz',
+                                    mmap_mode='r') if generated_ratio > 0.0 else None
+        flip = transforms.RandomHorizontalFlip()
+        c32 = transforms.RandomCrop(32, padding=4)
+        t = transforms.ToTensor()
+        
+        transforms_preprocess = transforms.Compose([t])
+        transforms_basic = transforms.Compose([flip, c32])
+        tf = style_transfer
+        transforms_augmentation = transforms.Compose([tf, transforms_preprocess])
+        generated_ratio = generated_ratio
+
+        #torch.manual_seed(seed)
+        #np.random.seed(seed)
+        #random.seed(seed)
+
+        # Prepare lists for combined data
+        images = [None] * target_size
+        labels = [None] * target_size
+        sources = [None] * target_size
+ 
+        if generated_dataset == None or generated_ratio == 0.0:
+            images, labels = zip(*base_trainset)
+            if isinstance(images[0], torch.Tensor):
+                images = TF.to_pil_image(images)
+            sources = [True] * len(base_trainset)
+        else:
+            num_generated = int(target_size * generated_ratio)
+            num_original = target_size - num_generated
+            # Create a single permutation for the whole epoch
+            original_perm = torch.randperm(len(base_trainset))
+            generated_perm = torch.randperm(len(generated_dataset['image']))
+
+            original_indices = original_perm[0:num_original]
+            generated_indices = generated_perm[0:num_generated]
+            generated_images = list(map(Image.fromarray, generated_dataset['image'][generated_indices]))
+            generated_labels = generated_dataset['label'][generated_indices]
+
+            original_subset = Subset(base_trainset, original_indices)
+            original_images, original_labels = zip(*original_subset)
+            if isinstance(original_images[0], torch.Tensor):
+                original_images = [TF.to_pil_image(img) for img in original_images if isinstance(img, torch.Tensor)]
+
+
+            # Transform and append original data
+            images[:num_original] = original_images
+            labels[:num_original] = original_labels
+            sources[:num_original] = [True] * num_original
+
+            # Append NPZ data
+            images[num_original:target_size] = generated_images
+            labels[num_original:target_size] = generated_labels
+            sources[num_original:target_size] = [False] * num_generated
+
+        return AugmentedDataset(images, labels, sources, transforms_preprocess,
+                                         transforms_basic, transforms_augmentation, transforms_generated,
+                                         robust_samples)
