bb33fbb18b8fe581f844ccb438fdf6ea3c5d1ecd
RojanRegmi <rjnrcks@gmail.com>
2024-10-20 07:56:55 +0200

    Gen Data Adaptations

--- gen_data_utils.py
+++ gen_data_utils.py
@@ -45,6 +45,7 @@ def load_augmented_traindata(base_trainset, target_size, style_transfer, seed=0,
         transforms_basic = transforms.Compose([flip, c32])
         tf = style_transfer
         transforms_augmentation = transforms.Compose([tf, transforms_basic, transforms_preprocess])
+        transforms_generated = transforms.Compose([transforms_generated, transforms_basic, transforms_preprocess])
         
 
         #torch.manual_seed(seed)
