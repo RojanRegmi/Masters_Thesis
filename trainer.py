2c66f17f24c5d59d1111cf96515979ec11f227cb
RojanRegmi <rjnrcks@gmail.com>
2024-11-05 11:24:49 +0100

    vgg reduced to layer 3

--- trainer.py
+++ trainer.py
@@ -307,13 +307,13 @@ if __name__ == '__main__' :
     random_choice_gen = RandomChoiceTransforms(transforms_list, probabilities_gen)
 
     transform_train = transforms.Compose([
-        #nst_transfer,
-        #transforms.RandomHorizontalFlip(),
-        #transforms.RandomCrop(32, padding=4),
+        nst_transfer,
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4),
         #random_choice_transform,
         #GeometricTrivialAugmentWide(),  
         #transforms.TrivialAugmentWide(),
-        #transforms.ToTensor(),
+        transforms.ToTensor(),
         #transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     
