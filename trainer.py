08c84516464a53ed3ef7fb3e88cd9217cf8e4913
RojanRegmi <rjnrcks@gmail.com>
2024-12-06 21:49:03 +0100

    trainer changes

--- trainer.py
+++ trainer.py
@@ -439,9 +439,9 @@ if __name__ == '__main__' :
             print(f'Target Size: {target_size}')
             #trainset = load_augmented_traindata(base_trainset=baseset, dataset=args.dataset, tf=transform_train, target_size=target_size, transforms_generated=transform_gen)
             augmented_trainset = AugmentedTrainDataLoader(base_trainset=baseset, dataset=args.dataset, tf=random_choice_transform, target_size=target_size, transforms_generated=random_choice_gen, batch_size=args.batch_size, generated_ratio=args.gen_nst_prob)
-            test_trainset = AugmentedTrainDataLoader(base_trainset=baseset, dataset=args.dataset, tf=None, target_size=target_size, transforms_generated=None, batch_size=args.batch_size, generated_ratio=args.gen_nst_prob)
+            #test_trainset = AugmentedTrainDataLoader(base_trainset=baseset, dataset=args.dataset, tf=None, target_size=target_size, transforms_generated=None, batch_size=args.batch_size, generated_ratio=args.gen_nst_prob)
             trainset = augmented_trainset.load_augmented_traindata(epoch=0)
-            temp_trainset = test_trainset.load_augmented_traindata(epoch=0)
+            #temp_trainset = test_trainset.load_augmented_traindata(epoch=0)
             print('Mixed Dataset Loaded')
         else:
             print('Loading Original CIFAR100')
@@ -455,7 +455,7 @@ if __name__ == '__main__' :
 
     #trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                            # shuffle=True, pin_memory=True, num_workers=4, worker_init_fn=seed_worker, generator=g)
-    trainloader = torch.utils.data.DataLoader(temp_trainset, batch_size=batch_size,
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=False, pin_memory=True, num_workers=4, worker_init_fn=seed_worker, generator=g)
     temp_trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=False, pin_memory=True, num_workers=4, worker_init_fn=seed_worker, generator=g)
